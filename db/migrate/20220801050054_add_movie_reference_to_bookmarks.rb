71ed1e89e8c5c92d585e3e65371c6dc32611d5ed
Annie Wang <annie9016@hotmail.com>
2022-08-02 14:55:04 +0800

    Fix migration file doing duplicate actions

--- db/migrate/20220801050054_add_movie_reference_to_bookmarks.rb
+++
@@ -1,5 +0,0 @@
-class AddMovieReferenceToBookmarks < ActiveRecord::Migration[7.0]
-  def change
-    add_reference :bookmarks, :movie, foreign_key: true
-  end
-end
--- db/migrate/20220801050500_add_list_reference_to_bookmarks.rb
+++
@@ -1,5 +0,0 @@
-class AddListReferenceToBookmarks < ActiveRecord::Migration[7.0]
-  def change
-    add_reference :bookmarks, :list, foreign_key: true
-  end
-end
